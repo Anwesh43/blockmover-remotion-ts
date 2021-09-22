a42845dd84d7e03f6ff8c7fbc981ef15f517e5e3
Anwesh43 <anweshthecool0@gmail.com>
2021-09-22 23:55:07 +0530

    using BlockMover in Video

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,16 +1,13 @@
 import {Composition} from 'remotion';
-import {HelloWorld} from './HelloWorld';
-import {Logo} from './HelloWorld/Logo';
-import {Subtitle} from './HelloWorld/Subtitle';
-import {Title} from './HelloWorld/Title';
+import BlockMover from './BlockMover';
 
 export const RemotionVideo: React.FC = () => {
 	return (
 		<>
 			<Composition
 				id="HelloWorld"
-				component={HelloWorld}
-				durationInFrames={150}
+				component={BlockMover}
+				durationInFrames={105}
 				fps={30}
 				width={1920}
 				height={1080}
@@ -18,34 +15,6 @@ export const RemotionVideo: React.FC = () => {
 					titleText: 'Welcome to Remotion',
 					titleColor: 'black',
 				}}
-			/>
-			<Composition
-				id="Logo"
-				component={Logo}
-				durationInFrames={200}
-				fps={30}
-				width={1920}
-				height={1080}
-			/>
-			<Composition
-				id="Title"
-				component={Title}
-				durationInFrames={100}
-				fps={30}
-				width={1920}
-				height={1080}
-				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: 'black',
-				}}
-			/>
-			<Composition
-				id="Subtitle"
-				component={Subtitle}
-				durationInFrames={100}
-				fps={30}
-				width={1920}
-				height={1080}
 			/>
 		</>
 	);
